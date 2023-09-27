81c5069baa3969050988d115919f85af16382700
aidangrigg <aidangrigg02@gmail.com>
2023-09-27 13:14:58 +1000

    feat: Added arg parser & some basic error handling

--- src/commandline.rs
+++ src/commandline.rs
@@ -1,7 +1,40 @@
-use std::io::{self, Write};
+use std::{io::{self, Write}, collections::HashSet};
+
+#[derive(Eq, Hash, PartialEq, Debug)]
+pub enum Args {
+    TemplateDir (String),
+    ProjectDir (String),
+}
+
+pub fn parse_args(args: Vec<String>) -> HashSet<Args> {
+
+    let mut parsed_args: HashSet<Args> = HashSet::new();
+
+    let mut args = args.iter();
+    args.next();
+
+    for arg in args {
+        if arg.starts_with("-t") {
+            let template_dir = arg.strip_prefix("-t").unwrap();
+
+            if template_dir.len() > 0  {
+                parsed_args.insert(Args::TemplateDir(String::from(template_dir)));
+            } else {
+                fatal_error(2, 
+                    "-t prefix used incorrectly. Correct syntax is \"-t<TEMPLATE-DIR> (without a space)\""
+                );
+            }
+        } else if arg.starts_with("-") {
+            fatal_error(1, &(String::from(arg) + " is not supported at this time"));
+        } else {
+            parsed_args.insert(Args::ProjectDir(String::from(arg)));
+        }
+    }
+
+    parsed_args
+}
 
 pub fn read_input(prompt: Option<&str>) -> String {
-
     match prompt {
         Some(prompt) => {
             print!("{}", prompt);
@@ -16,4 +49,15 @@ pub fn read_input(prompt: Option<&str>) -> String {
     stdio.read_line(&mut project_name).expect("Could not read from stdin");
 
     String::from(project_name.trim())
+}
+
+pub fn non_fatal_error(message: String, retry_attempts: u32, retry_fn: &dyn Fn()) {
+    unimplemented!();
+}
+
+pub fn fatal_error(code: i32, message: &str) {
+    print!("{}", message);
+    io::stdout().flush().ok();
+
+    std::process::exit(code);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,33 @@
+mod commandline;
 mod template;
 
 use template::Template;
 
-use std::path::Path;
+use commandline::Args;
+use std::{collections::HashSet, env, path::Path};
 
 fn main() {
-    let _t = Template::new(Path::new("templates"), Path::new("test"));
+    let args: HashSet<Args> = commandline::parse_args(env::args().collect());
+    let mut template_dir = String::new();
+    let mut project_dir = String::new();
+
+    for arg in args {
+        match arg {
+            Args::TemplateDir(dir) => template_dir = dir,
+            Args::ProjectDir(dir) => project_dir = dir,
+        }
+    }
+
+    if template_dir.is_empty() {
+        template_dir = commandline::read_input(Some(
+            "Please enter the directory of the template you would like to use: ",
+        ));
+    }
+
+    if project_dir.is_empty() {
+        project_dir =
+            commandline::read_input(Some("Please enter the directory of your new project: "));
+    }
+
+    let _t = Template::new(Path::new(&template_dir), Path::new(&project_dir));
 }
